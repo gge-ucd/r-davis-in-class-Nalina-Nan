8242906d2c2abc924ddb7836496fa9eb2df259a1
“nalina-nangit config --global user.name “nalina-nan <nalai@ucdavis.edu>
2019-02-12 17:31:52 -0800

    Week6

---
+++ script/w_5_assignment_DAM2.R
@@ -0,0 +1,52 @@
+library("tidyverse")
+
+surveys <- read_csv("data/portal_data_joined.csv")
+
+#Using tidyverse functions and pipes, subset to keep all the rows where weight is between 30 and 60, then print the first few (maybe… 6?) rows of the resulting tibble.
+weights_30to60 <- surveys %>% 
+  filter (weight<60) %>% 
+  filter(weight>30) %>% 
+
+#should not have pipe at the end
+#you can filter them in the same line using weight<60&weight>30
+  
+head(weights_30to60)
+
+
+#Make a tibble that shows the max (hint hint) weight for each species+sex combination, and name it biggest_critters. Use the arrange function to look at the biggest and smallest critters in the tibble (use ?, tab-complete, or Google if you need help with arrange).
+biggest_critters <- surveys %>%
+  group_by(species_id, sex) %>%
+  summarize(max_weight=max(weight, na.rm=T)) %>% 
+  arrange(desc(max_weight))
+
+smallest_critters <- surveys %>%
+  group_by(species_id, sex) %>%
+  summarize(min_weight=min(weight, na.rm=T)) %>% 
+  arrange(min_weight)
+
+#good point using na.rm and arrange
+#I am not sure I understood the command correctly, should we ask to find the smallest cricket in the biggest tibbie? if that so,arrange (asc(max_weigh)) would be more correct.... But when I run the code there are a lot on -inf. But thumb up on it, I could not reach this point in my code.
+
+#Try to figure out where the NA weights are concentrated in the data- is there a particular species, taxa, plot, or whatever, where there are lots of NA values? There isn’t necessarily a right or wrong answer here, but manipulate surveys a few different ways to explore this. Maybe use tally and arrange here.
+
+# NEED TO FINISH THIS PART
+
+
+
+#Take surveys, remove the rows where weight is NA and add a column that contains the average weight of each species+sex combination. Then get rid of all the columns except for species, sex, weight, and your new average weight column. Save this tibble as surveys_avg_weight. The resulting tibble should have 32,283 rows.
+surveys_avg_weight <- surveys %>% 
+  filter(!is.na(weight)) %>% 
+  group_by(species_id, sex) %>% 
+  mutate(avg_weight=mean(weight)) %>% 
+  select(species_id, sex, weight,avg_weight)
+#good poind using species_id instead of species 
+
+
+#Challenge: Take surveys_avg_weight and add a new column called above_average that contains logical values stating whether or not a row’s weight is above average for its species+sex combination (recall the new column we made for this tibble).
+surveys_avg_weight <- surveys_avg_weight %>% 
+   mutate(above_average = weight > avg_weight)
+
+
+#Extra Challenge: Figure out what the scale function does, and add a column to surveys that has the scaled weight, by species. Then sort by this column and look at the relative biggest and smallest individuals. Do any of them stand out as particularly big or small?
+
+# NEED TO FINISH THIS PART
---
+++ script/week6LiveCode.R
@@ -0,0 +1,180 @@
+library (tidyverse)
+
+surveys <- read_csv("data/portal_data_joined.csv")
+
+#take all NAs out of the weight, hindfoot_length, and sex columns
+
+surveys_complete <- surveys %>% 
+  filter(!is.na(weight),!is.na(hindfoot_length),!is.na(sex))
+
+#remove the species < 50 obsvervation
+
+species_counts<-surveys_complete %>% 
+  group_by(species_id) %>% 
+  tally() %>% #create new column name "n"
+  filter(n>=50)
+
+#%in%
+#match the list to show data of species that appear in species count (which we selected to have > 50)
+
+surveys_complete<-surveys_complete %>% 
+  filter(species_id %in% species_counts$species_id)
+
+#Select to look at some species
+species_keep <- c("DM","DO")
+
+surveys_complete %>% 
+  filter(surveys_complete$species_id %in% species_keep)
+
+#writing your dataframe to .scv
+
+write_csv(surveys_complete,path = "data_output/surveysComplete.csv")
+
+
+# 4# and a lot of sth... create a section
+#### ggplot =========== 
+
+#ts tab - current date and time
+# Tue Feb 12 14:31:20 2019 ------------------------------
+
+#painting from the blank canvas
+# adding on layer
+
+#ggplot(data=DATA,mapping=aes(HowToMapInCanvas))+
+#geom_function(putSthOnIt)
+
+ggplot(surveys_complete) 
+
+#define a mapping
+
+ggplot(data = surveys_complete, mapping = aes (x=weight,y=hindfoot_length)) + #define x and y axis # end with +
+  geom_point()
+
+#saving a plot object 
+survey_plot <- ggplot(data = surveys_complete, mapping = aes (x=weight,y=hindfoot_length))
+
+#save the same baseline canvas and adapt 
+survey_plot + 
+  geom_point()
+
+#anything in ggplot func, will be seen in geom_ you add
+
+# can modify each geom on its own 
+
+#challenge
+
+install.packages("hexbin")
+library(hexbin)
+survey_plot + 
+  geom_hex()
+#avoid overplotting
+
+#we're going to build points from ground up
+        
+#modifying whole geom appearances
+surveys_complete %>% 
+  ggplot (aes(x=weight,y=hindfoot_length)) +
+  geom_point(alpha=0.1,col="tomato") #alpha = transparency
+
+#using data in geom
+
+surveys_complete %>% 
+  ggplot(aes(x=weight,y=hindfoot_length))+
+  geom_point (alpha=0.1, aes (col=species_id)) #add color locally
+
+#putting color as a global aesthetic, every function after that still look at color
+surveys_complete %>% 
+  ggplot(aes(x=weight,y=hindfoot_length, col=species_id))+
+  geom_point (alpha=0.1)
+
+#using a little jitter
+surveys_complete %>% 
+  ggplot(aes(x=weight,y=hindfoot_length, col=species_id))+
+  geom_jitter (alpha=0.1) #jitter seperate them out a bit for a better view
+
+#moving on to boxplot
+surveys_complete %>% 
+  ggplot(aes(x=species_id,y=weight))+
+  geom_boxplot()
+
+#adding points to boxplot
+surveys_complete %>% 
+  ggplot(aes(x=species_id,y=weight))+
+  geom_boxplot() + 
+  geom_jitter(alpha=0.3, color = "salmon") #use jitter because point will too dense in the line
+
+# change the layer to show boxplot
+surveys_complete %>% 
+  ggplot(aes(x=species_id,y=weight))+
+  geom_jitter(alpha=0.3, color = "salmon")+
+  geom_boxplot(alpha = 0.2) #make fill more transparent
+#color #fill = inside
+
+#plotting time series
+yearly_count<-surveys_complete %>% 
+    count(year,species_id) #same as group by specie id&year, tally
+  
+yearly_count %>% 
+  ggplot(aes(x=year,y=n,group=species_id,color=species_id))+
+  geom_line()
+
+#facetting
+yearly_count %>% 
+  ggplot (aes(x=year,y=n,col=species_id))+
+  geom_line()+
+  facet_wrap(~species_id) #subplot(facet) by ~
+
+#including sex
+yearly_sex_counts<-surveys_complete %>% 
+  count(year,species_id,sex)
+
+yearly_sex_counts %>% 
+  ggplot(aes(x=year,y=n,color=sex))+
+  geom_line()+
+  facet_wrap(~species_id)+
+  theme_linedraw()+ #a lot of THEME lol
+  theme (panel.grid = element_blank())
+
+
+#save any stage
+
+ysx_plot <-yearly_sex_counts %>% 
+  ggplot(aes(x=year,y=n,color=sex))+
+  geom_line()+
+  facet_wrap(~species_id)
+
+ysx_plot<-
+  theme_minimal()
+
+#ggthemes::theme_tufle
+
+# alittle more facetting
+yearly_sex_weight<-surveys_complete %>% 
+  group_by(year,sex,species_id) %>% 
+  summarise(avg_weight=mean(weight))
+
+yearly_sex_weight %>% 
+  ggplot(aes(x = year, y = avg_weight, color = species_id)) +
+  geom_line() +
+  facet_grid(sex~.) # . = nothing, want to divided by
+
+# facet_grid uses rows ~ columns for facetting. the "." indicates nothing in this dimension
+
+#adding label and stuff 
+yearly_sex_counts %>% 
+  ggplot(aes(x = year, y = n, color = sex)) +
+  geom_line() +
+  facet_wrap(~ species_id) +
+  theme_bw() +
+  theme(panel.grid = element_blank()) +
+  labs(title="Observed species through time", x="Year of observation", y="Number of species")+
+  theme(text = element_text(size = 16)) +
+  theme(axis.text.x = element_text(color = "grey20", size = 12, angle = 90 , hjust = 0.5, vjust = 0.5))
+
+ggsave("figures/my_test_facet_plot.jpeg",height = 8, width = 8) #recognize the type of file you lastly saved
+
+#ggplot2Ref
+
+#source ("")#run the whole script into my env.
+
+#brcurrinder
